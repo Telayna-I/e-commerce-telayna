721e46e0f7b7925a05c69608d15ce7d8e6c8b44a
Telayna-i <telayna98@gmail.com>
2022-03-03 23:04:13 -0300

    Add functions to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,29 +2,42 @@ import './Cart.css'
 import { useContext } from 'react'
 import { CartContext } from '../../Context/CartContext'
 import ItemCart from '../ItemCart/ItemCart'
+import { NavLink } from 'react-router-dom'
 
 
 const Cart = () => {
 
-    const { cart, clearCart } = useContext(CartContext)
+    const { cart, clearCart, total } = useContext(CartContext)
     
     const handleCart = () =>{
         clearCart()
     }
 
-    return(
-        <div className = 'cart-container'>
-            <h2 className='title'>Carrito</h2>
-            <div className='buttons'>
-                <button className='vaciar-carrito' onClick ={handleCart}>Vaciar carrito</button>
+    if(cart.length > 0){
+        return(
+            <div className = 'cart-container'>
+                <h2 className='title'>Carrito</h2>
+                <div className='buttons'>
+                    <button className='vaciar-carrito' onClick ={handleCart}>Vaciar carrito</button>
+                </div>
+                <div className='cartt'>
+                    {cart.map((product,i)=>(
+                        <ItemCart className = "item-cart" product = {product} key = {i}  />
+                    ))}
+                </div>
+
+                <h4 className='precio-total' >El precio total es de: {total}</h4>
             </div>
-            <div className='cartt'>
-                {cart.map((product,i)=>(
-                    <ItemCart className = "item-cart" product = {product} key = {i}  />
-                ))}
+        )
+    }else{
+        return(
+            <div className='info'>
+                <h2 className='h2-info'>El carrito esta vacio</h2>
+                <NavLink to={'/'} className = "go-home" >Ir a Home</NavLink>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 export default Cart
